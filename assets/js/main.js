d0cf6de4551789dddf91214574a1323214f467d7
MCLuna <61129902+MCLuna@users.noreply.github.com>
2021-07-07 12:45:45 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+const categorias = document.querySelectorAll('#categorias .categoria');
+const contenedorPreguntas = document.querySelectorAll('.contenedor-preguntas');
+let categoriaActiva = null;
+
+categorias.forEach((categoria)=>{
+  categoria.addEventListener('click',(e)=>{
+    categorias.forEach((elemento)=>{
+      elemento.classList.remove('activa');
+    })
+    e.currentTarget.classList.toggle('activa');
+    categoriaActiva = categoria.dataset.categoria;
+
+    contenedorPreguntas.forEach((contenedor)=>{
+      if(contenedor.dataset.categoria === categoriaActiva){
+        contenedor.classList.add('activo');
+      }else{
+        contenedor.classList.remove('activo');
+      }
+    })
+  })
+});
+
+const preguntas = document.querySelectorAll('.preguntas .contenedor-pregunta');
+preguntas.forEach((pregunta)=>{
+  pregunta.addEventListener('click',(e)=>{
+    e.currentTarget.classList.toggle('activa');
+
+    const respuesta = pregunta.querySelector('.respuesta');
+    const alturaReal = respuesta.scrollHeight;
+
+    if(!respuesta.style.maxHeight){
+      respuesta.style.maxHeight = alturaReal + 'px';
+    }else{
+      respuesta.style.maxHeight = null;
+    }
+
+    preguntas.forEach((elemento)=>{
+      if(pregunta !== elemento){
+        elemento.classList.remove('activa');
+        elemento.querySelector('.respuesta').style.maxHeight = null;
+      }
+    });
+  });
+});
